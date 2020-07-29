5b5ae7030be7fa198df78dc6c5fa86e2cd4bd0a6
joncomstock <comstock2004@gmail.com>
2020-07-29 16:56:50 -0600

    Initial creationg of FunctionListGoals Lambda

--- cdk/lib/master-fullstack-single-stack.ts
+++ cdk/lib/master-fullstack-single-stack.ts
@@ -1,9 +1,40 @@
 import * as cdk from '@aws-cdk/core';
+import lambda = require('@aws-cdk/aws-lambda');
+import { Duration } from '@aws-cdk/core';
+import s3 = require('@aws-cdk/aws-s3');
+import { BucketAccessControl } from '@aws-cdk/aws-s3';
+
+export interface MasterProps extends cdk.StackProps {
+  projectName: string;
+}
 
 export class MasterFullStackSingleStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props: MasterProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const lambdaBucket = new s3.Bucket(this, 'AssetsBucket', {
+      accessControl: BucketAccessControl.PRIVATE,
+      // metricsConfigurations: [
+      //   {
+      //     "id": "EntireBucket"
+      //   }
+      // ],
+      // websiteConfiguration: {
+      //   "indexDocument": "index.html"
+      // },
+  });
+
+    new lambda.Function(this, 'FunctionListGoals', {
+      functionName: `${props.projectName}-ListGoals`,
+      runtime: lambda.Runtime.NODEJS_12_X,
+      description: 'Get list of goals for userId',
+      handler: 'index.handler',
+      memorySize: 256,
+      timeout: Duration.seconds(120),
+      //role
+      //environment
+      // fix this key
+      code: lambda.S3Code.fromBucket(lambdaBucket, "key")
+    });
   }
 }
