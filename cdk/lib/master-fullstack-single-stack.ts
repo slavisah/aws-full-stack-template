7be71d7fdbd4d0558971fa86302995a734f09894
cyclops2020 <scott.tate@blndspt.com>
2020-07-29 16:53:28 -0600

    added dynamodb objects

--- cdk/lib/master-fullstack-single-stack.ts
+++ cdk/lib/master-fullstack-single-stack.ts
@@ -1,9 +1,42 @@
 import * as cdk from '@aws-cdk/core';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import iam = require('@aws-cdk/aws-iam');
+import { RemovalPolicy } from '@aws-cdk/core';
+
 
 export class MasterFullStackSingleStack extends cdk.Stack {
+
+  /*DynamoDb*/
+  private readonly ProjectName: string = 'MyGoalsProject';
+  private readonly TableName: string = 'Goals';
+  /*Lambda*/
+
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    /* Create DynamoDB Goals Table */
+    const goalsTable = new dynamodb.Table(this, 'TGoals', {
+      tableName: `${this.ProjectName}-${this.TableName}`,
+      partitionKey: { name: 'userId', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'goalId', type: dynamodb.AttributeType.STRING },
+      readCapacity: 1,
+      writeCapacity: 1,
+      removalPolicy: RemovalPolicy.DESTROY
+    });
+
+    /* Create DynamoDB Role/Policy */
+    const dynamoDbRole = new iam.Role(this, 'DynamoDbRole', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+    });
+    const goalsPolicy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ['dynamodb:*'],
+      resources: [goalsTable.tableArn]
+    });
+    dynamoDbRole.addToPolicy(goalsPolicy);
+  
+   
+    
+
   }
 }
